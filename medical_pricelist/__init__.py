d2b1a7fafc6ac8f6304f45e55046a517f2a3f395
Xavier Gillmann <xgillmann@bluesquarehub.com>
2019-10-11 17:09:46 +0200

    default security setup

---
+++ medical_pricelist/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'medical_pricelist.apps.MedicalPricelistConfig'
--- medical_pricelist/apps.py
+++ medical_pricelist/apps.py
@@ -1,5 +1,22 @@
 from django.apps import AppConfig
 
+MODULE_NAME = "medical_pricelist"
+
+DEFAULT_CFG = {
+    "gql_query_pricelists_perms": []
+}
+
 
 class MedicalPricelistConfig(AppConfig):
-    name = 'medical_pricelist'
+    name = MODULE_NAME
+
+    gql_query_pricelists_perms = []
+
+    def _configure_permissions(self, cfg):
+        MedicalPricelistConfig.gql_query_pricelists_perms = cfg[
+            "gql_query_pricelists_perms"]
+
+    def ready(self):
+        from core.models import ModuleConfiguration
+        cfg = ModuleConfiguration.get_or_default(MODULE_NAME, DEFAULT_CFG)
+        self._configure_permissions(cfg)
