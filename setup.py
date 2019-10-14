425f8388bc79f3c3ed5e3501f689ddaff4c3cfa2
Xavier Gillmann <xgillmann@bluesquarehub.com>
2019-10-14 10:58:15 +0200

    bumped v0.0.5

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='openimis-be-medical_pricelist',
-    version='0.0.4',
+    version='0.0.5',
     packages=find_packages(),
     include_package_data=True,
     license='GNU AGPL v3',
