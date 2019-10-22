9186882bffe9e541ed9a27e2168678178da0b849
Xavier Gillmann <xgillmann@bluesquarehub.com>
2019-10-22 14:22:42 +0200

    technical documentation (README) & code cleaning

--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@ setup(
         'django',
         'django-db-signals',
         'djangorestframework',
+        'openimis-be-medical',
     ],
     classifiers=[
         'Environment :: Web Environment',
@@ -31,7 +32,6 @@ setup(
         'License :: OSI Approved :: GNU Affero General Public License v3',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
     ],
