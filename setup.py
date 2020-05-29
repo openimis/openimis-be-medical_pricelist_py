99cca9470dffb3915376a8788c5cf8260e5e6b36
Xavier Gillmann <xgillmann@bluesquarehub.com>
2020-05-29 18:41:19 +0200

    github workflow + bump 1.2.0-rc1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='openimis-be-medical_pricelist',
-    version='1.1.0',
+    version='1.2.0-rc1',
     packages=find_packages(),
     include_package_data=True,
     license='GNU AGPL v3',
