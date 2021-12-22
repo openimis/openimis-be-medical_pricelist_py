4b1ba4f15daf95b950128f8e48b20d73cb0e137d
Eric Darchis <darchis@pobox.com>
2021-12-22 15:18:49 +0100

    v1.3.2

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='openimis-be-medical_pricelist',
-    version='1.3.1',
+    version='1.3.2',
     packages=find_packages(),
     include_package_data=True,
     license='GNU AGPL v3',
