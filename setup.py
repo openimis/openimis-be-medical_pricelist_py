446e95815509f598049a935d57a534c73986bf0e
Patrick Delcroix <patrick.delcroix@swisstph.ch>
2022-04-29 17:25:52 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='openimis-be-medical_pricelist',
-    version='1.3.0',
+    version='1.4.0',
     packages=find_packages(),
     include_package_data=True,
     license='GNU AGPL v3',
