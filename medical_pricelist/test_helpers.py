7995d190fd0d7d448a619b2eb3f018ab9d1cbf58
sniedzielski <sniedzielski@soldevelo.com>
2022-05-30 12:55:20 +0200

    OTC-607: added create item/service pricelist helpers for batch-run tests

--- medical_pricelist/test_helpers.py
+++ medical_pricelist/test_helpers.py
@@ -1,5 +1,36 @@
 from location.models import HealthFacility
-from medical_pricelist.models import ServicesPricelistDetail, ItemsPricelistDetail
+from medical_pricelist.models import (
+    ItemsPricelist,
+    ServicesPricelist,
+    ServicesPricelistDetail,
+    ItemsPricelistDetail
+)
+
+
+def create_test_item_pricelist(location_id, custom_props={}):
+    return ItemsPricelist.objects.create(
+        **{
+            "name": "test-item-price-list",
+            "location_id": location_id,
+            "pricelist_date": "2019-01-01",
+            "validity_from": "2019-01-01",
+            "audit_user_id": -1,
+            **custom_props
+        }
+    )
+
+
+def create_test_service_pricelist(location_id, custom_props={}):
+    return ServicesPricelist.objects.create(
+        **{
+            "name": "test-item-price-list",
+            "location_id": location_id,
+            "pricelist_date": "2019-01-01",
+            "validity_from": "2019-01-01",
+            "audit_user_id": -1,
+            **custom_props
+        }
+    )
 
 
 def add_service_to_hf_pricelist(service, hf_id=18, custom_props={}):
