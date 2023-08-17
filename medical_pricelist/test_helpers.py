f5a55f487319a7d4bd200bcba6fdab0c6b36fa59
jdolkowski <jdolkowski@soldevelo.com>
2023-08-17 10:39:23 +0200

    OP-1494: add test helepers

--- medical_pricelist/test_helpers.py
+++ medical_pricelist/test_helpers.py
@@ -45,6 +45,14 @@ def add_service_to_hf_pricelist(service, hf_id=18, custom_props={}):
         }
     )
 
+def update_service_in_hf_pricelist(service_id, custom_props={}):
+    service_pricelist_detail = ServicesPricelistDetail.objects.get(service_id=service_id)
+    service_pricelist_detail.save_history()
+    for key, value in custom_props.items():
+        if hasattr(service_pricelist_detail, key):
+            setattr(service_pricelist_detail, key, value)
+    return service_pricelist_detail.save()
+
 
 def add_item_to_hf_pricelist(item, hf_id=18, custom_props={}):
     hf = HealthFacility.objects.get(pk=hf_id)
@@ -57,3 +65,11 @@ def add_item_to_hf_pricelist(item, hf_id=18, custom_props={}):
             **custom_props
         }
     )
+
+def update_item_in_hf_pricelist(item_id, custom_props={}):
+    item_pricelist_detail = ItemsPricelistDetail.objects.get(item_id=item_id)
+    item_pricelist_detail.save_history()
+    for key, value in custom_props.items():
+        if hasattr(item_pricelist_detail, key):
+            setattr(item_pricelist_detail, key, value)
+    return item_pricelist_detail.save()
